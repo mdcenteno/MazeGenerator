b54c16d38841c6d8c1ea731219427205396d8e26
Marco Centeno <marcodurencenteno@yahoo.com.br>
2025-06-24 20:30:56 -0300

    Refactor .NET Framework -> .NET 8
    * Refatorado ConsoleApplication .NetFramework ->.Net 8
    
    * Correção array[][]
    
    ---------
    
    Co-authored-by: Marco Duren Centeno <marcodurencenteno@msn.com>

---
+++ MazeGeneratorConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using Generator;
+
+namespace MazeGeneratorConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Maze2Screen();
+
+            //Maze2File();
+        }
+
+        private static void Maze2File()
+        {
+            Maze maze = new Maze(10, 10);
+            maze.Generate(true);
+            Console.WriteLine(maze.ToString());
+            File.AppendAllText($"{Directory.GetCurrentDirectory()}/maze.txt", maze.ToString());
+        }
+
+        private static void Maze2Screen()
+        {
+            Maze maze = new Maze(10, 10);
+            maze.Generate(true);
+            Console.WriteLine(maze.ToString());
+        }
+    }
+}
